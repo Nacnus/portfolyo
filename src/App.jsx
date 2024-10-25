2943afcce0e4ae5001e4de2bf18a6744098439ae
Yusuf Can Sun <yusufcansun6@gmail.com>
2024-10-25 18:37:03 +0300

    added font-family

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import './App.css'
 import Header from "./components/Header.jsx";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Home from "./pages/Home.jsx";
-import Projects from "./pages/Projects.jsx";
-import Contact from "./pages/Contact.jsx";
-import Cv from "./pages/Cv.jsx";
+import Home from "./components/Home.jsx";
+import Contact from "./components/Contact.jsx";
+import Cv from "./components/Cv.jsx";
 import MyCarousels from "./carousel/MyCarousels.jsx";
 import PageContainer from "./container/PageContainer.jsx";
-import MyTypeWriters from "./typeWriters/MyTypeWriters.jsx";
 
 const theme = createTheme({
     palette: {
         primary: { main: "#FF6500", contrastText: "#000000" },
-        secondary: { main: "#0B192C", contrastText: "#fff" }
-    }
+        secondary: { main: "#0B192C", contrastText: "#fff" },
+    },
+    typography: {
+        fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
+    },
 });
 
 function App() {
   return (
       <ThemeProvider theme={theme}>
           <Header/>
+          <Home/>
           <PageContainer className="content">
-              <Home/>
               <MyCarousels/>
               <Contact/>
               <Cv/>
           </PageContainer>
-          <MyTypeWriters/>
       </ThemeProvider>
 
   )
