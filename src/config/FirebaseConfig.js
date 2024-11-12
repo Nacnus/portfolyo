b0e7631b58d4dadb4e2a9ae09098dcadc5452263
Yusuf Can Sun <yusufcansun6@gmail.com>
2024-11-13 02:59:40 +0300

    added google auth

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,6 +1,7 @@
-
 import { initializeApp } from "firebase/app";
 import {getFirestore, collection,addDoc} from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 
 
 // Your web app's Firebase configuration
@@ -16,6 +17,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
 
 export async function submitContactForm(name, mail, message) {
         await addDoc(collection(db, "contactForm"), {
