bd11eec9494a70a42cd8f394145f976021fbe95b
Yusuf Can Sun <yusufcansun6@gmail.com>
2024-11-13 01:40:01 +0300

    added anchor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Tabs, Tab } from "@mui/material";
-import '../css/Header.css';  // Stil dosyanı koruyoruz
+import '../css/Header.css';
 
 function Header() {
-    const [value, setValue] = useState(0); // Aktif sekmeyi kontrol eden state
+    const [value, setValue] = useState(0);
 
     const headers = [
         {
@@ -20,33 +20,48 @@ function Header() {
         },
         {
             section: 'contact',
-            title: 'İletişim'
+            title: 'İLETİŞİM'
         },
-
     ];
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            headers.forEach((header, index) => {
+                const section = document.getElementById(header.section);
+                const sectionTop = section.offsetTop;
+                const sectionHeight = section.offsetHeight;
+
+                if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+                    setValue(index);
+                }
+            });
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <div className='header'>
-            <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between',alignItems: 'center' }}>
-                <div className='left-text'>Yusuf Can Sun</div>
+            <Container maxWidth="lg" sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <a href={"#home"} className="left-text" style={{textDecoration: 'none', color: 'inherit', cursor: 'pointer'}}>
+                    Yusuf Can Sun
+                </a>
                 <div>
                     <Tabs
                         value={value}
                         onChange={handleChange}
                         textColor="inherit"
                         indicatorColor="secondary"
-
+                        sx={{paddingTop:'12px'}}
                     >
                         {headers.map((header) => (
-                            <Tab
-                                key={header.section}
-                                label={header.title}
-                                href={`#${header.section}`}
-                            />
+                            <Tab key={header.section} label={header.title} href={`#${header.section}`} />
                         ))}
                     </Tabs>
                 </div>
