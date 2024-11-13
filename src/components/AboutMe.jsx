c565f0a449aad92a3f15c339ad170f14feaaf602
Yusuf Can Sun <yusufcansun6@gmail.com>
2024-11-13 13:57:44 +0300

    added my photo

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,29 +2,62 @@ import React from 'react';
 import {Button, Card, CardActions, CardContent, Typography} from "@mui/material";
 import { IoMdDownload } from "react-icons/io";
 import cvFile from "../Cv/Cv.pdf"
+import photo from "../images/photo.jpeg"
 
 function AboutMe() {
     return (
         <Card id='aboutMe' elevation={4} className={'rounded-4'}>
             <CardContent>
-                <Typography
-                    variant="h2"
-                    className='d-flex justify-content-center'
-                    fontWeight='bold'
-                    component="div"
-                >
-                    Hakkımda
-                </Typography>
+                <div style={{
+                    display: "flex",
+                    flexDirection: 'row',
+                    alignItems: 'flex-start',
+                    justifyContent: 'space-between',
+                    marginBottom: '20px',
+                    marginTop:'20px'
+                }}>
+                    <div>
+                        <img
+                            src={photo}
+                            alt="Yusuf Can Sun"
+                            style={{
+                                width: '150px',
+                                height: '150px',
+                                borderRadius: '50%',
+                                objectFit: 'cover',
+                                justifyContent: 'start',
+                                alignSelf: 'flex-start',
+                                marginLeft: '15px',
+                                border: '2px solid rgba(0,0,0,0.5)'  // Add this line for a thin black border
+                            }}
+                        />
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        flex: 1,
+                        marginRight: '150px'
+                    }}>
+                        <Typography
+                            variant="h2"
+                            fontWeight="bold"
+                            component="div"
+                            style={{textAlign: 'center'}}
+                        >
+                            Hakkımda
+                        </Typography>
 
-                <Typography
-                    variant="h5"
-                    className='d-flex justify-content-center'
-                    fontWeight='lighter'
-                    component="caption"
-                >
-                    Front-end Developer
-                </Typography>
-
+                        <Typography
+                            variant="h5"
+                            fontWeight="lighter"
+                            component="caption"
+                            style={{textAlign: 'center'}}
+                        >
+                            Front-end Developer
+                        </Typography>
+                    </div>
+                </div>
                 <Typography
                     className='d-flex justify-content-start mt-2 mb-2 p-4'
                     variant='h6'
@@ -54,7 +87,7 @@ function AboutMe() {
                     className={'rounded-4'}
                     color='secondary'
                     size='large'
-                    startIcon={<IoMdDownload />}
+                    startIcon={<IoMdDownload/>}
                 >
                     Cv İndirmek İçin Tıklayın
                 </Button>
