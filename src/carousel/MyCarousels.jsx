86aaa383d6aca4ffb01febab441031aa8c43c5ed
Yusuf Can Sun <yusufcansun6@gmail.com>
2024-11-13 13:57:15 +0300

    fixed carousel bug

--- src/carousel/MyCarousels.jsx
+++ src/carousel/MyCarousels.jsx
@@ -9,38 +9,42 @@ import {Card} from "@mui/material";
 
 function MyCarousel() {
     return (
-        <Card className="carousel-container rounded-4" id='projects' elevation={5}>
+        <Card className="carousel-container" style={{overflow:'visible'}} id='projects' elevation={3}>
             <Carousel
                 prevIcon={<CiCircleChevLeft color='#FF9100FF' fontSize='300%' />}
                 nextIcon={<CiCircleChevRight color='#FF9100FF' fontSize='300%'  />}
                 className="mt-5"
             >
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={image1}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption className='carousel-caption-custom'>
-                        <h3>Clone LinkedIn Projesi</h3>
-                        <p>Bu projede Linkedin'in clonenunu yaptım.</p>
-                    </Carousel.Caption>
+                    <a href="https://github.com/Nacnus/linkedIn-clone" target="_self">
+                        <img
+                            className="d-block w-100"
+                            src={image1}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption className='carousel-caption-custom'>
+                            <h3>Clone LinkedIn Projesi</h3>
+                            <p>Bu projede Linkedin'in clonenunu yaptım.</p>
+                        </Carousel.Caption>
+                    </a>
                 </Carousel.Item>
 
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={image2}
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption className='carousel-caption-custom'>
-                        <h3>E-Ticaret Projesi</h3>
-                        <p>Kendi E-ticaret sitemi yaptım.</p>
-                    </Carousel.Caption>
+                    <a href="https://github.com/Nacnus/e-commerce-nuxt" target="_self">
+                        <img
+                            className="d-block w-100"
+                            src={image2}
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption className='carousel-caption-custom'>
+                            <h3>E-Ticaret Projesi</h3>
+                            <p>Kendi E-ticaret sitemi yaptım.</p>
+                        </Carousel.Caption>
+                    </a>
                 </Carousel.Item>
             </Carousel>
         </Card>
-    );
+);
 }
 
 export default MyCarousel;
